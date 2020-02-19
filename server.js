394efc69520a56b639256bfeaec745ae557caec6
igor <embiggenerd@gmail.com>
2020-02-19 16:36:55 -0500

    seeds working and selecting from cars works

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express')
 
+const db = require('./db')
+
 const server = express()
 
 server.use(express.json())
+
+server.get('/cars', async (req, res, next) => {
+    try {
+        const cars = await db('cars')
+            .select('*')
+        res.json(cars)
+    } catch (e) {
+        next(e)
+    }
+})
 
 server.use((err, req, res, next) => {
     console.log(err)
